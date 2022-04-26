c39a0f462bb4ece2a75818223e04ceae8e473d8d
Jerzor <jerzor2000@vp.pl>
2022-04-26 10:58:54 +0200

    change css to scss, write single test for App component

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import App from './App'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+test('renders the component', () => {
+  render(<App />)
+  const text = screen.getByText('PetsGo')
+  expect(text).toBeInTheDocument()
+})
