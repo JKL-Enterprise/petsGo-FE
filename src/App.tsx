d855345bbf339bf598d3d04df8c61991da1da671
Jerzor <jerzor2000@vp.pl>
2022-04-30 13:30:34 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Navigation } from './components'
+import { Route, Routes } from 'react-router-dom'
+import {
+  HomePage as Home,
+  LoginPage as Login,
+  RegisterPage as Register,
+} from './view'
 
 const App = () => {
   return (
-    <>
+    <div className="mx-auto w-3/4">
       <Navigation />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
   )
 }
 
