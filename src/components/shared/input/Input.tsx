28cf0e7caea8a095772495fd4eb98c06f9abc741
jerzor <jerzor2000@vp.pl>
2022-05-05 12:43:41 +0200

    change Input component

--- src/components/shared/input/Input.tsx
+++ src/components/shared/input/Input.tsx
@@ -3,7 +3,7 @@ import './Input.scss'
 
 interface InputProps {
   name: string
-  text: string
+  label: string
   type?: 'text' | 'number' | 'password' | 'email'
   placeholder?: string
   value: string
@@ -12,16 +12,15 @@ interface InputProps {
 
 export const Input = ({
   name,
-  text,
+  label,
   type = 'text',
-  placeholder = text,
+  placeholder = label,
   value,
   onChange,
 }: InputProps) => {
   return (
     <div className="inputContainer">
       <input
-        id={name}
         name={name}
         type={type}
         className="input"
@@ -30,7 +29,7 @@ export const Input = ({
         onChange={onChange}
       />
       <label htmlFor={name} className="input_label">
-        {text}
+        {label}
       </label>
     </div>
   )
