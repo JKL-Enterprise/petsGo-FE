2ea2f922de650b1fd2dcba0e443e8bd49c896799
Jerzor <jerzor2000@vp.pl>
2022-05-28 23:19:14 +0200

    create user authentication context to save access token

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,21 @@ import {
 } from './view'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
+import UserUserAuthenticationProvider from './context/UserAuthenticationProvider'
 
 const App = () => {
   return (
-    <div className="mx-auto w-3/4">
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      <ToastContainer />
-    </div>
+    <UserUserAuthenticationProvider>
+      <div className="mx-auto w-3/4">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        <ToastContainer />
+      </div>
+    </UserUserAuthenticationProvider>
   )
 }
 
---
+++ src/context/UserAuthenticationProvider.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, useState } from 'react'
+
+interface UserUserAuthenticationProviderProps {
+  children: JSX.Element | JSX.Element[]
+}
+
+export interface Model {
+  accessToken: string
+  refreshToken: string
+  changeAccessToken: (arg: string) => void
+  changeRefreshToken: (arg: string) => void
+}
+
+const initialValues: Model = {
+  accessToken: '',
+  refreshToken: '',
+  changeAccessToken: () => {},
+  changeRefreshToken: () => {},
+}
+
+export const UserUserAuthenticationContext = createContext(initialValues)
+
+const UserAuthenticationProvider = ({
+  children,
+}: UserUserAuthenticationProviderProps) => {
+  const [accessToken, setAccessToken] = useState<string>('')
+  const [refreshToken, setRefreshToken] = useState<string>('')
+
+  const changeAccessToken = (accessToken: string) => setAccessToken(accessToken)
+  const changeRefreshToken = (refreshToken: string) =>
+    setRefreshToken(refreshToken)
+
+  return (
+    <UserUserAuthenticationContext.Provider
+      value={{
+        accessToken,
+        refreshToken,
+        changeAccessToken,
+        changeRefreshToken,
+      }}
+    >
+      {children}
+    </UserUserAuthenticationContext.Provider>
+  )
+}
+
+export default UserAuthenticationProvider
